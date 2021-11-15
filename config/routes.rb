8dc4aa85eddd5dab2ad64d3da406b48d8353ba62
Carlos Hernandez Topete <chtopete@gmail.com>
2021-11-15 16:15:20 +0900

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :decks, only: [:new, :create, :index, :show] do
+    resources :rentals, only: [:new, :create, :index]
+  end
+  resources :rentals, only: [:edit, :update]
 end
