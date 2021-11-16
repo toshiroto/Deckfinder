38ab1406433415b6010740526be0ac18e07bd202
Carlos Hernandez Topete <chtopete@gmail.com>
2021-11-16 12:45:45 +0900

    manual user seeds updated without email

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,20 +14,19 @@ puts 'database is clean'
 puts 'creating Decks'
 puts 'creating Users'
 
-joss_dixon = User.new(name: "Joss Dixon", email: "josscdixon@gmail.com", password: "12345")
+joss_dixon = User.new(email: "jossdixon@gmail.com", password: "123456")
 joss_dixon.save!
-michiharu_ono = User.new(name: "michiharu Ono", email: "michiharuono77gmail.com", password: "67890")
+michiharu_ono = User.new(email: "michiharuono77@gmail.com", password: "567890")
 michiharu_ono.save!
-carlos_topete = User.new(name: "Carlos Topete", email: "chtopete@gmail.com", password: "12309")
+carlos_topete = User.new(email: "chtopete@gmail.com", password: "123098")
 carlos_topete.save!
 
   deck_instance = Deck.create(
     name: Faker::Books::CultureSeries.book,
     desc: Faker::Quotes::Chiquito.joke,
     price: rand(1..100),
-    user_id: user_instance.id
+    user_id: joss_dixon.id
   )
-  puts "User #{user_instance.id} is created"
-  puts "Deck #{deck_instance.id} is created"
+
 
 puts 'done!'
