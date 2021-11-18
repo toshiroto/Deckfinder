796e912bbfb4a6885e03150fce5b8b8c73a7035a
Michiharu Ono <michiharuono77@gmail.com>
2021-11-18 12:38:21 +0900

    logic fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :rentals
+  has_many :rented_decks, through: :rentals, source: :deck
+  has_many :owned_decks, class_name:"Deck"
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
